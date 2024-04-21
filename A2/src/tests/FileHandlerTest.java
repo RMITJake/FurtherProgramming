4a83fb08250caed1b450b36b68b4a94ed4e5d66e
RMITJake <s3905550@student.rmit.edu.au>
2024-04-21 23:00:59 +1000

    added comments to filehandertest:

--- A2/src/tests/FileHandlerTest.java
+++ A2/src/tests/FileHandlerTest.java
@@ -8,29 +8,42 @@ import java.util.List;
 public class FileHandlerTest extends FileHandler {
     private String testFile;
 
+///////////
+// Setup //
+///////////
     @Before()
     public void setUp(){
         testFile = "files/inputTest.csv";
     }
+// END Setup
 
+
+////////////////////////
+// getLineFromCSVTest //
+////////////////////////
+@Test()
+public void getLineFromCSVTest() throws FileNotFoundException{
+    String input = testFile;
+    String expected = "Home,4000,relax; watch tv,Indoor,100";
+    List<List<String>> fileContents = getLineFromCSV(input);
+    String fileString = "";
+    for(List<String> outerStr : fileContents){
+        for(String innerStr : outerStr){
+            fileString += innerStr + ",";
+        }
+    }
+    fileString = fileString.substring(0, fileString.length()-1);
+    
+    assertEquals(expected, fileString);
+}
+// END getLineFromCSVTest
+
+/////////////////////////////////////////////
+// getLineFromCSVFileNotFoundExceptionTest //
+/////////////////////////////////////////////
     @Test(expected=FileNotFoundException.class)
-    public void getLineFromCSVNullPointerExceptionTest() throws FileNotFoundException{
+    public void getLineFromCSVFileNotFoundExceptionTest() throws FileNotFoundException{
         getLineFromCSV("files/404.csv");
     }
-
-    @Test()
-    public void getLineFromCSVTest() throws FileNotFoundException{
-        String input = testFile;
-        String expected = "Home,4000,relax; watch tv,Indoor,100";
-        List<List<String>> fileContents = getLineFromCSV(input);
-        String fileString = "";
-        for(List<String> outerStr : fileContents){
-            for(String innerStr : outerStr){
-                fileString += innerStr + ",";
-            }
-        }
-        fileString = fileString.substring(0, fileString.length()-1);
-
-        assertEquals(expected, fileString);
-    }
+// END getLineFromCSVFileNotFoundExceptionTest
 }
