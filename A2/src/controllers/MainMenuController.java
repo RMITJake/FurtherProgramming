9aadf7e5c72b1ba5c3e40afa53d4a6d212b7f7f3
RMITJake <s3905550@student.rmit.edu.au>
2024-05-04 13:57:20 +1000

    added mouse handler to event table:

--- A2/src/controllers/MainMenuController.java
+++ A2/src/controllers/MainMenuController.java
@@ -1,12 +1,17 @@
 package src.controllers;
 // Java library imports
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.chart.PieChart.Data;
+import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+
 import java.util.List;
+import javafx.collections.ObservableList;
 // Local imports
 import src.handlers.DatabaseHandler;
 import src.handlers.DebugHandler;
@@ -25,9 +30,13 @@ public class MainMenuController {
     @FXML private TableColumn venueNameColumn;
     @FXML private TableColumn venueCompatibilityColumn;
 
-    @SuppressWarnings("unchecked")
     @FXML private void initialize(){
         DebugHandler.print("IN MAINMENU.initialize");
+        loadRequestTable();
+        loadVenueTable();
+    }
+
+    private void loadRequestTable(){
         requestNoColumn.setCellValueFactory(new PropertyValueFactory<Event, Integer>("id"));
         eventColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("title"));
         artistColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("artist"));
@@ -35,11 +44,21 @@ public class MainMenuController {
 
         List<Event> eventList = DatabaseHandler.readEventsTable();
         requestTable.setItems(FXCollections.observableArrayList(eventList));
+    }
 
+    private void loadVenueTable(){
         venueNoColumn.setCellValueFactory(new PropertyValueFactory<Venue, Integer>("id"));
         venueNameColumn.setCellValueFactory(new PropertyValueFactory<Venue, String>("name"));
-        venueCompatibilityColumn.setCellValueFactory(new PropertyValueFactory<Venue, String>("category"));
+        // Compatibility column intentionally left blank on first load
+
         List<Venue> venueList = DatabaseHandler.readVenuesTable();
         venueTable.setItems(FXCollections.observableArrayList(venueList));
+    }
+
+    @FXML private void selectEvent(MouseEvent mouseEvent){
+        Event event = requestTable.getSelectionModel().getSelectedItem();
+        if(event != null){
+            DebugHandler.print("clicked " + event.getArtist());
+        }
     }
 }
